ca88c3a64fb8b53c38c8809430dee3908e73b200
james.sullivan <james.sullivan@axomic.com>
2024-08-19 22:58:20 -0400

    finished up about and little contact section for now

--- src/Components/About/AboutContainer/MainContent/MainContent.jsx
+++ src/Components/About/AboutContainer/MainContent/MainContent.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import "./MainContent.css"
+import { useState } from 'react'
 
 export default function MainContent() {
+
+  const [clicked,setClicked] = useState(false)
+  let emailClass = ''
+  clicked ? emailClass = "contact-text" : emailClass = "contact-text not-clicked"
+  
   return (
     <div className='about-main-content'>
-        <p>This is just a place for me on the internet that is not Instagram,
-        and I'll share things about projects I'm working on. Sometimes music,
+        <p>This is just a place for me on the internet that is not Instagram.
+        I share my projects as I am constantly learning.... Sometimes music,
         sometimes games, sometimes just some pictures from my life</p>
         <br/>
         <p>If you want to contact me:</p>
-        <div className='contact'>
-        <p className='contact-text'>
+        <div className='contact' >
+        <p className={emailClass} onClick={() => setClicked((prevState) => !prevState)}>
             jsullivan2847@gmail.com
         </p>
         </div>
