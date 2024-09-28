43a9725a1a2f5a85e1158aa6d08e075774b3cc37
jsullivan2847 <jsullivan2847@gmail.com>
2024-09-28 16:58:56 -0400

    fixed empty a tag

--- src/Projects/ga_demo_reel.jsx
+++ src/Projects/ga_demo_reel.jsx
@@ -10,8 +10,7 @@ export default function ga_demo_reel() {
   return (
     <div className='flex'>
         <p>Game audio demo reel I made while studying at <a className='external-link' href='https://school.videogameaudio.com/apply/'>School of Video Game Audio</a></p>
-        {/* <br/> */}
-        <p>Made with <a className='external-link' href='https://www.fmod.com/'></a>FMOD</p>
+        <p>Made with <a className='external-link' href='https://www.fmod.com/'>FMOD</a></p>
         <iframe className='youtube-link' src="https://www.youtube.com/embed/KHAkyUKpOX4?si=Dm5qSeTetWn2RsNB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   )
