8a258e1a7c950316a7384c7ba7211e132ff5f723
jsullivan2847 <jsullivan2847@gmail.com>
2024-09-28 17:24:26 -0400

    Started first blog post

--- src/Entries/entry_1.jsx
+++ src/Entries/entry_1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./entries.css"
 
 export const metadata = {
-  categories: ["Text","Images","Video","Audio"],
+  categories: ["Text", "Images", "Video", "Audio"],
   title: "First Post",
   date: "Aug/11/2024"
 }
@@ -11,8 +11,34 @@ export default function entry_1() {
 
   return (
     <div className='content-container'>
-      <h1>Title</h1>
+      <h1>That's True</h1>
       <p>Content</p>
+      <img src='https://f4.bcbits.com/img/a2208395273_16.jpg' alt='' />
+      <br/>
+      <div>
+        <div>I can't find it on the map - someplace I won't know till I get there. Where's my
+          buddy when I need him? My bad, I miss everyone I know.<br />
+          <br />
+          Yeah it's true. I do got you. Yeah it's true. <br />
+          <br />
+          Lucky not to have to mow the lawn. Trying not to be so goddamn dramatic. Singing somebody else's songs - hope to
+          see some familar faces. Missing what I got before it's gone - things I shouldn't be taking for granted.<br />
+          <br />
+          Yeah it's true. I do got you. Yeah it's true. This is not new. Yeah it's true.
+        </div>
+
+        <h3>credits</h3>
+        <div>
+
+          releases October 5, 2024
+
+          <br />Vocals - Melissa Caren<br />
+          Guitar - Ryan Hagan<br />
+          All other instrumentation / production / engineering / mixing - James Sullivan<br />
+          Mastering - Sam Hall<br />
+          Artwork - Tyler Moore
+        </div>
+      </div>
     </div>
   )
 }
