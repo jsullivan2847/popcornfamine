9da7b9d3f96d670bd4df20b04edae40d5bb2fcc6
jsullivan2847 <jsullivan2847@gmail.com>
2025-02-21 19:24:54 -0500

    fixed header

--- src/Components/HeaderContainer/Header/Header.jsx
+++ src/Components/HeaderContainer/Header/Header.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import './Header.css'
 
 export default function Header() {
+  let headercontent = <p style={{}}>Home</p>;
+  console.log("location: ", window.location.pathname)
+  if(window.location.pathname !== "/"){
+    headercontent = <a href="/">Home</a>
+  }
   return (
     <div className='title'>
-        <a href='/'>popcornfamine</a>
+        {headercontent}
     </div>
   )
 }
